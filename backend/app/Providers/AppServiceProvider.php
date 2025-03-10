c0879767144758e9f6208915af3bb592c0bba248
pasindu-dil <dilshanpasindu357@gmail.com>
2025-03-10 11:47:52 +0530

    Implement Laravel Passport for API authentication; configure token expiration and add Passport routes

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Passport::enableImplicitGrant();
+        Passport::tokensExpireIn(now()->addHours(10));
+        Passport::refreshTokensExpireIn(now()->addHours(6));
     }
 }
